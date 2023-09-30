9030406a92c0ab7f0252d77e54a8ebcd84e30185
ribeiro valentin <Portable>
2023-09-30 16:39:57 +0200

    Implement Bootstrap for minimalist design and display all events on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+
+    public function showListEvents(
+        EventRepository $eventRepository
+    ): Response {
+
+        $user = $this->getUser();
+        $events = $eventRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'events' => $events,
+            'user' => $user,
         ]);
+
     }
+
 }
